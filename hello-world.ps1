5fec46744463f888e319d37edfed980d446a3b79
RajeshKumarM <52698898+rajesh929@users.noreply.github.com>
2021-04-06 16:37:36 +0530

    Update hello-world.ps1

--- hello-world.ps1
+++ hello-world.ps1
@@ -1,3 +1,3 @@
 Write-Host "Hello-World!"
 Write-Host "This is a sample file for Git Repo"
-Write-Host "This line added to check CI in Azure pipeline"
+Write-Host "This line added to check CI in Azure pipeline today"
